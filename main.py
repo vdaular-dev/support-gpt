c8f4dab7b25b0128f3fb5a484c85122e63783f88
Ryan Harmon <ryan@bloomcu.com>
2023-05-18 13:54:22 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import pickle
+
+from dotenv import load_dotenv
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from llama_index import GPTVectorStoreIndex, download_loader
+
+load_dotenv()
+os.environ.get('OPENAI_API_KEY')
+
+# Authenticate our Google account to discover Google Docs.
+def authorize_gdocs():
+    google_oauth2_scopes = [
+        "https://www.googleapis.com/auth/documents.readonly"
+    ]
+    cred = None
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", 'rb') as token:
+            cred = pickle.load(token)
+    if not cred or not cred.valid:
+        if cred and cred.expired and cred.refresh_token:
+            cred.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", google_oauth2_scopes)
+            cred = flow.run_local_server(port=0)
+        with open("token.pickle", 'wb') as token:
+            pickle.dump(cred, token)
+
+if __name__ == '__main__':
+    authorize_gdocs()
+    GoogleDocsReader = download_loader('GoogleDocsReader')
+    gdoc_ids = ['1qmG5KtGh7eanbgM_2fHagz11uhJ-USlHiZQs1nlSJxA']
+    loader = GoogleDocsReader()
+    documents = loader.load_data(document_ids=gdoc_ids)
+    index = GPTVectorStoreIndex.from_documents(documents)
+
+    while True:
+        prompt = input('Type your prompt...')
+        query_engine = index.as_query_engine()
+        response = query_engine.query(prompt)
+        print(response)
+
+        # Get the last token usage
+        # last_token_usage = index.llm_predictor.last_token_usage
+        # print(f"last_token_usage={last_token_usage}")
